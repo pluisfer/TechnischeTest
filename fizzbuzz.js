894919b043deaf0cd4ba2c17f4e667e4777a7b96
Luis F Pacheco <pluisfer@gmail.com>
2021-05-03 16:54:44 -0500

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,30 @@
+function fizzbuzz(num) {
+  if (typeof num !== "number") {
+    return "Error: the argument must be a number";
+  }
+  const divisible = (divisor, num) => num % divisor === 0;
+
+  if (num === 0) {
+    return 0;
+  }
+  if (divisible(3, num) && divisible(5, num)) {
+    return "fizzbuzz";
+  }
+  if (divisible(3, num)) {
+    return "fizz";
+  }
+  if (divisible(5, num)) {
+    return "buzz";
+  }
+  return num;
+}
+
+function print(num) {
+  for (i = 0; i <= num; i++) {
+    console.log(`${i}: ${fizzbuzz(i)}`);
+  }
+}
+
+print(16);
+
+module.exports = fizzbuzz;
